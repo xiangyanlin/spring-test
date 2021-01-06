067efaa0974e026aa476dbbcc6b2331280bbfbe2
向延琳 <2356150341@qq.com>
2021-01-06 14:22:19 +0800

    init

---
+++ src/main/java/com/xiangyanlin/springtest/SpringTestApplication.java
@@ -0,0 +1,16 @@
+package com.xiangyanlin.springtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xyl
+ */
+@SpringBootApplication
+public class SpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestApplication.class, args);
+    }
+
+}
